d4ed42d0c9cc538a55a0319666a3ad0cf70d2279
Dishant Arora <dishantarora16@gmail.com>
2023-10-24 21:25:01 +0530

    added list item component

---
+++ src/elements/ListItem.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Pressable, StyleSheet, View, Text } from "react-native";
+
+interface IListItem {
+  title: string;
+  type?: string;
+  onPress?: () => {};
+}
+
+const ListItem = ({ title, type, onPress }: IListItem) => {
+  return (
+    <Pressable onPress={onPress}>
+      <View style={styles.container}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.type}>{type}</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+export default ListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#000",
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    padding: 20,
+    margin: 10,
+  },
+  title: {
+    color: "#fff",
+    fontSize: 18,
+  },
+  type: { color: "#fff" },
+});
